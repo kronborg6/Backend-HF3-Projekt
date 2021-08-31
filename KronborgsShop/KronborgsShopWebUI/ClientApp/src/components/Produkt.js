4437169aa8690e265efac8a00ae586f63563f54f
Kronborg <mkronborg7@gmail.com>
2021-08-31 08:31:57 +0200

    0.7.0
    add WebUI

---
+++ KronborgsShop/KronborgsShopWebUI/ClientApp/src/components/Produkt.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Produkt extends Component {
+  static displayName = Produkt.name;
+
+  render () {
+    return (
+      <div>
+       
+      </div>
+    );
+  }
+}
